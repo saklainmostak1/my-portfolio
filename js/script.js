a8133cbae13371a9b4027c2b60161c7c16a10c63
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-12-10 19:37:12 +0600

    slide

--- js/script.js
+++ js/script.js
@@ -4,3 +4,32 @@ var typed = new Typed('.typing', {
     BackSpeed: 30,
     loop: true
 })
+
+const nav = document.querySelector('.nav'),
+navList = nav.querySelectorAll('li')
+allSection = document.querySelectorAll('.section')
+totalSection = allSection.length
+totalNavList = navList.length
+for(let i = 0; i<totalNavList; i++)
+{
+    // console.log(navList[i]);
+    const a = navList[i].querySelector('a')
+    console.log(a)
+    a.addEventListener('click', function(){
+        for(let j = 0; j<totalNavList; j++)
+        {
+            navList[j].querySelector('a').classList.remove('active')
+        }
+        this.classList.add('active')
+        showSection(this)
+    })
+}
+function showSection(element){
+    for(let i = 0; i < totalSection; i++){
+        allSection[i].classList.remove('active')
+    }
+  const target =  element.getAttribute('href').split('#')[1];
+  console.log(target);
+  document.querySelector('#' + target).classList.add('active')
+
+}
