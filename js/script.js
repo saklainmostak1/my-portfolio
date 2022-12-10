d9f122019c28451577a525d4fa24520f8132923f
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-12-10 21:14:21 +0600

    download resume

--- js/script.js
+++ js/script.js
@@ -15,16 +15,17 @@ for(let i = 0; i<totalNavList; i++)
     // console.log(navList[i]);
     const a = navList[i].querySelector('a')
     console.log(a)
-    a.addEventListener('click', function(){
-        for(let i = 0; i < totalSection; i++){
-            allSection[i].classList.remove('back-section')
-        }
+    a.addEventListener('click', function()
+    {
+        // for(let i = 0; i < totalSection; i++){
+        //     allSection[i].classList.remove('back-section')
+        // }
         for(let j = 0; j<totalNavList; j++)
         {
-            if(navList[j].querySelector('a').classList.contains('active'))
-            {
-                allSection[j].classList.add('back-section');
-            }
+            // if(navList[j].querySelector('a').classList.contains('active'))
+            // {
+            //     allSection[j].classList.add('back-section');
+            // }
             navList[j].querySelector('a').classList.remove('active')
         }
         this.classList.add('active')
